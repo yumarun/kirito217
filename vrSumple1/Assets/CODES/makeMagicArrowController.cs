5d47b4a3fd609bebb68cae188363d08473eef44a
umaruchan <yuta.wota.0048@gmail.com>
2020-02-25 12:43:26 +0900

    ステージ床少し付け足し arrowをprafab化して一定時間ごとに生成、発射されるようにした

---
+++ vrSumple1/Assets/CODES/makeMagicArrowController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeMagicArrowController : MonoBehaviour
+{
+    public GameObject MagicArrowPrefab;
+    public bool StartMakeArrow = false;
+    float timeKeeper = 0;
+    float IntervalMakeArrow = 1.0f;
+    GameObject button1;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        button1 = GameObject.Find("b1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartTimeKeeper += Time.deltaTime;
+        if (button1.GetComponent<button1>().canmove)
+        {
+            timeKeeper += Time.deltaTime;
+        }
+        if (timeKeeper >= IntervalMakeArrow)
+        {
+            GameObject arrow = Instantiate(MagicArrowPrefab) as GameObject;
+            arrow.transform.position = new Vector3(-44, Random.Range(3.0f, 5.0f), Random.Range(-3.0f, 0));
+            timeKeeper = 0;
+        }
+    }
+}
