d50cb2b1142227faf8823fdec8ac7848e92d6818
umaruchan <yuta.wota.0048@gmail.com>
2020-03-18 18:45:13 +0900

    音追加

---
+++ vrSumple1/Assets/Script/Music/SwordMagicCollisionSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordMagicCollisionSound : MonoBehaviour
+{
+    public AudioClip SwordAndMagicCollision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("leftarrow");
+        }
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Magic")
+        {
+            GetComponent<AudioSource>().PlayOneShot(SwordAndMagicCollision);
+            Debug.Log("atatta");
+        }
+    }
+    
+}
