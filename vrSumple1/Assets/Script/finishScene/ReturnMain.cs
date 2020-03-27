dd86daf4173b8bf46aa44801f6fd25436d0eb320
umaruchan <yuta.wota.0048@gmail.com>
2020-03-28 05:18:44 +0900

    finishシーン作成(mainシーン→finishシーンの移行はまだ)

---
+++ vrSumple1/Assets/Script/finishScene/ReturnMain.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnMain : MonoBehaviour
+{
+    public bool CanPushBbutton = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanPushBbutton && OVRInput.GetDown(OVRInput.Button.Two)) {
+            SceneManager.LoadScene("main1");
+        }
+    }
+}
---
+++ vrSumple1/Assets/Script/finishScene/ScreenBreaker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBreaker : MonoBehaviour
+{
+    float timeKeeper = 0;
+    public float MovieStopTime = 42.5f;
+    public GameObject Screen1;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeKeeper += Time.deltaTime;
+        if (timeKeeper >= MovieStopTime)
+        {
+            GetComponent<ReturnMain>().CanPushBbutton = true;
+            Destroy(Screen1);
+        }
+    }
+}
